5a9444cd17937881d946a8e6259a99a56e7226b8
kanizAlam <gsoftecprop@gmail.com>
2022-09-07 11:07:05 +0530

    client js change

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId:'gudijivg',
+  projectId:process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: 'skEHy15FlU0H9pCcWUi7hL1Px1Z6da8rPiFY4o4mggU3UDrPrZKrWFGSrg7EC9jvegsxH0leFloW9dQpLfc7U28xl3yqfAQjd24MpSFtCdzVEEx1lMAEGDDHWCdywhcHCXC7nlNsuZBuq9MmO3RGGj0GMmUBq3Sa0hhFXrCVitLMdgQAQhIW',
+  token:process.env.REACT_APP_SANITY_TOKEN
 });
 
 const builder = imageUrlBuilder(client);
